5d69006cc1c2801cbf5fc12bd98fc6f82a4b1660
Ngoc Tu <nntu079@gmail.com>
2020-10-26 14:31:51 +0700

    test test

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ posts = [
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Jane Gov Doe',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
